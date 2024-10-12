ae516225b4fb821a6259de7d08e96b79a0cf0bdd
carpetbakery <20468196+CarpetBakery@users.noreply.github.com>
2024-10-12 16:47:32 -0400

    don't print dialogue while we're already printing

--- scripts/DialogManager.gd
+++ scripts/DialogManager.gd
@@ -6,9 +6,10 @@ class_name DialogManager extends Node
 @onready var speaker: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 # Export vars
-@export var speed : float = 10
+@export var speed : float = 0.02
 
-var delay : float = 0
+var delay : float = 0		## For pausing text
+var printing: bool = false	## Are we currently printing?
 
 func _ready() -> void:
 	# Don't show any characters
@@ -24,7 +25,12 @@ func hideText():
 	dialogLabel.visible_characters = 0
 
 func printText():
-	while true:
+	# Don't try to start printing again if we're already doing it
+	if printing:
+		return
+	
+	printing = true
+	while printing:
 		# Add a letter to the end
 		dialogLabel.visible_characters += 1
 		
@@ -33,6 +39,7 @@ func printText():
 		
 		# Exit if we're finshed printing the string
 		if dialogLabel.visible_ratio >= 1:
+			printing = false
 			break
-		
 		await get_tree().create_timer(speed, false).timeout
+	print_debug("Finshed printing")
