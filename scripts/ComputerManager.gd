0af518333b07721a0fd9a6ed661bd486d8fe2fcb
carpetbakery <20468196+CarpetBakery@users.noreply.github.com>
2024-10-17 16:45:58 -0400

    WORKS!!!!

--- scripts/ComputerManager.gd
+++ scripts/ComputerManager.gd
@@ -1,23 +1,23 @@
 class_name ComputerManager extends Node
 
 # Nodes
-@export var cursor: Sprite2D
 @export var viewport: SubViewport
 @export var cam: Camera2D
+
+# Nodes in this scene
+@onready var cursor: Sprite2D = $cursor
+
 
 func _ready() -> void:
 	# Hide the mouse cursor
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	
 
 func _process(delta: float) -> void:
 	# Set the cursor to the mouse position
-	#cursor.position = get_viewport().get_mouse_position()
-	#cursor.position = viewport.get_mouse_position()
-	#cursor.position = cam.get_local_mouse_position()
-	#print(cam.get_local_mouse_position())
-	print(viewport.get_mouse_position())
 	cursor.position = floor(viewport.get_mouse_position())
+	# Clamp cursor position inside screen
+	cursor.position.x = clamp(cursor.position.x, 0, 640-1)
+	cursor.position.y = clamp(cursor.position.y, 0, 480-1)
 	
 	if Global.DEBUG_MODE:
 		if Input.is_action_just_pressed("ui_cancel"):
--- scripts/ComputerWindow.gd
+++ scripts/ComputerWindow.gd
@@ -1,6 +1,13 @@
 class_name ComputerWindow extends Sprite2D
 
-@export var dragBox: CollisionShape2D
+@export var cursorArea: Area2D
+@export var dragArea: Area2D
+@export var closeArea: Area2D
+
+@onready var whiteFlash: Sprite2D = $WhiteFlash
+
+# Hovering on close
+var mouseOnClose: bool = false
 
 # Dragging
 var mouseOnDrag: bool = false
@@ -11,20 +18,32 @@ var mouseStartPos: Vector2
 var texSize: Vector2
 
 # Viewport size
-var viewSize = Vector2(853, 480)
-
+var viewSize: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	texSize = texture.get_size()
-
+	viewSize = get_viewport().size
+	whiteFlash.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	mouseOnClose = false
+	mouseOnDrag = false
+	
+	# Check mouse collisions
+	for i in cursorArea.get_overlapping_areas():
+		if i == dragArea:
+			mouseOnDrag = true
+		if i == closeArea:
+			mouseOnClose = true
+	
 	if Input.is_action_just_pressed("mouseLeft"):
 		if mouseOnDrag:
 			dragging = true
 			mouseStartPos = position - get_viewport().get_mouse_position()
+		if mouseOnClose:
+			hideWindow()
 	
 	if not Input.is_action_pressed("mouseLeft"):
 		dragging = false
@@ -32,19 +51,35 @@ func _process(delta: float) -> void:
 	# Dragging behaviour
 	if dragging:
 		position = mouseStartPos + get_viewport().get_mouse_position()
-		print(position)
 	else:
 		# Snap back to the inside of the screen
 		var xpad = (viewSize.x - 640) / 2
+		xpad = 0
 		position.x = clamp(position.x, xpad + texSize.x/2, viewSize.x - xpad - texSize.x/2)
 		position.y = clamp(position.y, texSize.y/2, viewSize.y - texSize.y/2)
-		#position.y = clamp(position.y, texSize.y/2, viewSize.y - texSize.y)
-		pass
+	
+	if Input.is_action_just_pressed("jump"):
+		showWindow()
+	
+	# Round position
+	position = floor(position)
 
+## Show the window
+func showWindow():
+	if visible:
+		return
+	
+	show()
+	whiteFlash.show()
+	await get_tree().create_timer(0.2, false).timeout
+	whiteFlash.hide()
+	
 
-func _on_area_2d_mouse_entered() -> void:
-	mouseOnDrag = true
-
-
-func _on_area_2d_mouse_exited() -> void:
-	mouseOnDrag = false
+## Hide the window
+func hideWindow():
+	if not visible:
+		return
+	
+	whiteFlash.show()
+	await get_tree().create_timer(0.08, false).timeout
+	hide()
