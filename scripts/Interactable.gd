906b96cd63f1372b606b99b3e52ddb620e021671
carpetbakery <20468196+CarpetBakery@users.noreply.github.com>
2024-10-18 19:26:28 -0400

    tried scene loading

--- scripts/Interactable.gd
+++ scripts/Interactable.gd
@@ -3,6 +3,8 @@ class_name Interactable extends Area3D
 @export var meshInst: MeshInstance3D
 @export var interactionText: String
 
+#const MP_ROOM = preload("res://maps/mpRoom.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,4 +16,7 @@ func _process(delta: float) -> void:
 
 func onInteract(player: PlayerFps):
 	print("Interacted!!")
+	#get_tree().change_scene_to_packed(MP_ROOM)
+	#get_tree().change_scene_to_file("res://maps/mpRoom.tscn")
+	
 	pass
