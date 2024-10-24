8e497287e77585a2b6fa59c970b9272afa665260
carpetbakery <20468196+CarpetBakery@users.noreply.github.com>
2024-10-24 00:40:39 -0400

    imported new assets

--- scripts/interaction/Door.gd
+++ scripts/interaction/Door.gd
@@ -3,3 +3,15 @@ class_name Door extends Interactable
 func onInteract(_player: PlayerFps):
 	_player.addDialog("I need to turn off the DRILLS.")
 	_player.startDialog()
+
+func _day0(_player: PlayerFps):
+	pass
+
+func _day1(_player: PlayerFps):
+	pass
+
+func _day2(_player: PlayerFps):
+	pass
+
+func _day3(_player: PlayerFps):
+	pass
