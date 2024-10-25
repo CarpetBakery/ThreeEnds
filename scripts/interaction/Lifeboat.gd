75ab375f91b3bee96dc3f1821613d27a334d4074
carpetbakery <20468196+CarpetBakery@users.noreply.github.com>
2024-10-25 15:06:01 -0400

    done hopefully

--- scripts/interaction/Lifeboat.gd
+++ scripts/interaction/Lifeboat.gd
@@ -1,5 +1,7 @@
 class_name Lifeboat extends HoldInteract
 
+@export var audioFade: AnimationPlayer
+
 var interactCount = 0
 
 func onInteract(_player: PlayerFps):
@@ -7,5 +9,27 @@ func onInteract(_player: PlayerFps):
 		_player.addDialog("Hm... I forgot this was here.")
 		_player.startDialog()
 	elif interactCount == 1:
+		_player.addDialog("I wonder if it still works...")
+		_player.startDialog()
+	elif interactCount == 2:
+		_player.addDialog("Wouldn't hurt to take it for a spin.")
+		_player.startDialog()
+	elif interactCount == 3:
+		_player.addDialog("Here I go!")
+		_player.startDialog()
 		
-		pass
+		await _player.dialogFinished
+		
+		# Fade out, ending 3 of 3
+		_player.startTransition(false, 2)
+		audioFade.play("fadeAudio")
+		await _player.hud.animationPlayer.animation_finished
+		
+		await get_tree().create_timer(2).timeout
+
+		_player.hud.credits.play("credits2")
+		await _player.hud.credits.animation_finished
+		
+		Global.resetGame()
+	
+	interactCount += 1
